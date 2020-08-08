4b8c9a641515513d50ca1a0a9d90cba5d90653e4
Iminzah <sharroniminzah@gmail.com>
2020-08-08 17:41:17 +0300

    linking pages

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvRegister.setOnClickListener {
+            val intent=Intent(baseContext,RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+     btnLogin.setOnClickListener{
+         var userName=etUsername.text.toString()
+         var password=etPassword.text.toString()
+         Toast.makeText(baseContext,password,Toast.LENGTH_SHORT).show()
+
+     }
     }
 }
